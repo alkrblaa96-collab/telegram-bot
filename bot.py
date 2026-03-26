4a89083df9e5efb44f6cce43b9f868bb18a073d3
alkrblaa96-collab <alkrblaa96@gmail.com>
2026-03-26 03:56:30 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import telebot
+
+TOKEN = "7668280359:AAGKEgb6Gd_j5CTLq8xmKSQKkxFqpGzr4D0"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "هلا حبيبي البوت شغال 🔥")
+
+bot.infinity_polling()
